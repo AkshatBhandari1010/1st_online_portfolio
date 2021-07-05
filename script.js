cc9396a99e7209831b74b6b115d43786ba3dadb4
AkshatBhandari1010 <67556390+AkshatBhandari1010@users.noreply.github.com>
2021-07-06 04:32:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Hamburger menu
 const hamburger = document.getElementById("hamburger");
 const hiddenmenu = document.getElementById("hiddenmenu");
 
@@ -5,3 +6,38 @@ hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('show');
     hiddenmenu.classList.toggle('show');
 });
+
+//Smooth scroll
+const scroll = new SmoothScroll('a[href*="#"]');
+
+//Copy text
+const gmail = document.getElementById('gmail');
+const num = document.getElementById('num');
+
+gmail.addEventListener('click', function() {
+    copygmail();
+});
+function copygmail(){
+    const textarea = document.createElement('textarea');
+    textarea.setAttribute('readonly', '');
+    textarea.style.position = 'absolute';
+    textarea.value = gmail.innerText;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('Copy');
+    document.body.removeChild(textarea);
+};
+
+num.addEventListener('click', function() {
+    copynum();
+});
+function copynum(){
+    const textarea = document.createElement('textarea');
+    textarea.setAttribute('readonly', '');
+    textarea.style.position = 'absolute';
+    textarea.value = num.innerText;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('Copy');
+    document.body.removeChild(textarea);
+};
